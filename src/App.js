0e956ff139ca16c7e9b312210543016958f019c8
coolzyte <coolzyte@gmail.com>
2019-02-28 17:21:05 +0100

    completed the highlight section

--- src/App.js
+++ src/App.js
@@ -9,10 +9,7 @@ import Highlight from "./components/highlights";
 class App extends Component {
   render() {
     return (
-      <div
-        className="App"
-        style={{ height: "1500px", background: "cornflowerblue" }}
-      >
+      <div className="App" style={{ height: "1500px" }}>
         <Header />
         <Featured />
         <VenueNfo />
--- src/components/highlights/Discount.js
+++ src/components/highlights/Discount.js
@@ -3,18 +3,36 @@ import Fade from "react-reveal/Fade";
 import Slide from "react-reveal/Slide";
 
 class Discount extends Component {
+  state = {
+    discountStart: 0,
+    discountEnd: 30
+  };
+
+  percentage = () => {
+    if (this.state.discountStart < this.state.discountEnd) {
+      this.setState({
+        discountStart: this.state.discountStart + 1
+      });
+    }
+  };
+
+  componentDidUpdate() {
+    setTimeout(() => {
+      this.percentage();
+    }, 30);
+  }
   render() {
     return (
       <div className="center_wrapper">
         <div className="discount_wrapper">
-          <Fade>
+          <Fade onReveal={() => this.percentage()}>
             <div className="discount_porcentage">
-              <span>23%</span>
+              <span>{this.state.discountStart}%</span>
               <span>OFF</span>
             </div>
           </Fade>
 
-          <Slide>
+          <Slide right>
             <div className="discount_description">
               <h3>Purchase tickets before 20th JUNE</h3>
               <p>Sed ut perspiciatis unde omnis iste natu</p>
