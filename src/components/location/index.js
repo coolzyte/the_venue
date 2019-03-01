66a4da985bd28fca024572aea6c76588242c5fd0
coolzyte <coolzyte@gmail.com>
2019-03-01 09:02:50 +0100

    added the googlemap and footer section

---
+++ src/components/location/index.js
@@ -0,0 +1,20 @@
+import React from "react";
+
+const Location = () => {
+  return (
+    <div className="location_wrapper">
+      <iframe
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2396.431468267328!2d3.950863407845982!3d7.410905306095324!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x10399328b3ce4a63%3A0xd5ddde362f8e86e0!2sCoolzyte!5e0!3m2!1sen!2sng!4v1551426242184"
+        width="100%"
+        height="500px"
+        frameBorder="0"
+        allowFullScreen
+      />
+      <div className="location_tag">
+        <div>Location</div>
+      </div>
+    </div>
+  );
+};
+
+export default Location;
